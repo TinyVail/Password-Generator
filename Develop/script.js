01fb291b2b6c69249a412ac168562f2a352974fe
ClancyGilroy <78936857+TinyVail@users.noreply.github.com>
2021-03-19 15:33:04 -0500

    declared the variables and created the arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,20 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate").addEventListener("click", writePassword);
+
+//Listing the nums and characters arrays
+
+var specialCharacters = ["%", "!", "&", ",", "*", "-", "+", ".", "/", "<", ">", "?","~"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// declare the future variables i'll use
+var confirmLength = "";
+var confirmspecialCharacters;
+var confirmalphaLower;
+var confirmalphaUpper;
+var confirmNumbers;
+
 
 // Write password to the #password input
 function writePassword() {
